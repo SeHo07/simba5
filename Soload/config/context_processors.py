7a8bd5d8caa40c0b4104f76524c5bc741b68131a
SeHo07 <61018913+SeHo07@users.noreply.github.com>
2026-06-24 02:29:29 +0900

    appkey 분리

--- Soload/config/context_processors.py
+++ Soload/config/context_processors.py
@@ -1,9 +1,9 @@
+from django.conf import settings
 from main.models import Profile
 
 def global_data(request):
-    data = {'BRAND_NAME': 'Soload'}
+    data = {'BRAND_NAME': 'Soload', 'KAKAO_APP_KEY': settings.KAKAO_APP_KEY}
     if request.user.is_authenticated:
         profile, created = Profile.objects.get_or_create(user=request.user)
         data['user_profile'] = profile
     return data
-
